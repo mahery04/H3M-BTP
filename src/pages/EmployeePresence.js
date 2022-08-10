5b2470ae617c6b79c579fb7d1dd99c8d8f55bd3c
mahery04 <mahery0405@gmail.com>
2022-08-10 16:19:51 +0200

    Fea: Add component Presence

--- src/pages/EmployeePresence.js
+++ src/pages/EmployeePresence.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
+import dailyEmployeeService from '../services/dailyEmployeeService';
+
+import { useParams } from 'react-router-dom';
 
 function EmployeePresence() {
+
+    const initialEmployeeState = {
+        id:           null,
+        matricule:    '',
+        firstname:    '',
+        lastname:     ''
+    }
+
+    const findEmployee = useParams();
+    const employee_id = findEmployee.id 
+    const [loaded, setLoaded] = useState(false)
+    const [dailyemployee, setDailyemployee] = useState(initialEmployeeState)
+
+
+    useEffect(() => {
+        const load = async () => {
+          const res = await dailyEmployeeService.get(employee_id)
+          console.log("RES LAVA BE ", res.data);
+          setDailyemployee(res.data)
+          setLoaded(true)
+        }
+        if (employee_id && !loaded) {
+          load();
+        }
+    }, [employee_id, loaded])
+
   return (
     <div>
         <Button 
@@ -14,6 +47,13 @@ function EmployeePresence() {
         <Typography variant="h3" sx={{ px: 5, mt: 1, mb: 5 }}>
             Listes des présences des employés
         </Typography>
+        <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+                <b>N° Matricule : </b>{dailyemployee.matricule} <br/>
+                <b>NOM : </b> {dailyemployee.firstname} <br/>
+                <b>Prénom : </b> {dailyemployee.lastname} <br/>
+            </CardContent>
+        </Card>
     </div>
   )
 }
