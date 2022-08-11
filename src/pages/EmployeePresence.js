2f3b0f5716815ea5c408206c634bcf602789c584
mahery04 <mahery0405@gmail.com>
2022-08-11 14:18:16 +0200

    Fix: Create table for presence

--- src/pages/EmployeePresence.js
+++ src/pages/EmployeePresence.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
@@ -7,89 +8,149 @@ import CardContent from '@mui/material/CardContent';
 
 import dailyEmployeeService from '../services/dailyEmployeeService';
 
-import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import { useTable } from 'react-table';
 
-import Table from "./Table";
+import makeData from './makeData';
 
-import axios from 'axios';
+const Styles = styled.div`
+  padding: 1rem;
+
+  table {
+    border-spacing: 0;
+    border: 1px solid black;
+
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+`
+function Table({ columns, data }) {
+  // Use the state and functions returned from useTable to build your UI
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({
+    columns,
+    data,
+  })
+
+  // Render the UI for your table
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row)
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
 
 function EmployeePresence() {
 
-    const initialEmployeeState = {
-        id:           null,
-        matricule:    '',
-        firstname:    '',
-        lastname:     ''
-    }
-
-    const findEmployee = useParams();
-    const employee_id = findEmployee.id 
-    const [loaded, setLoaded] = useState(false)
-    const [dailyemployee, setDailyemployee] = useState(initialEmployeeState)
-    const [data, setData] = useState([]);
-
-    useEffect(() => {
-        const load = async () => {
-          const res = await dailyEmployeeService.get(employee_id)
-          console.log("RES LAVA BE ", res.data);
-          setDailyemployee(res.data)
-          setLoaded(true)
-        }
-        if (employee_id && !loaded) {
-          load();
-        }
-    }, [employee_id, loaded])
-
-    useEffect(() => {
-        (async () => {
-          const result = await axios("https://api.tvmaze.com/search/shows?q=snow");
-          setData(result.data);
-        })();
-    }, []);
-
-    const columns = useMemo(
-        () => [
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Name',
+        columns: [
           {
-            // first group - TV Show
-            Header: "TV Show",
-            // First group columns
-            columns: [
-              {
-                Header: "Name",
-                accessor: "show.name"
-              },
-              {
-                Header: "Type",
-                accessor: "show.type"
-              }
-            ]
+            Header: 'First Name',
+            accessor: 'firstName',
           },
           {
-            // Second group - Details
-            Header: "Details",
-            // Second group columns
-            columns: [
-              {
-                Header: "Language",
-                accessor: "show.language"
-              },
-              {
-                Header: "Genre(s)",
-                accessor: "show.genres"
-              },
-              {
-                Header: "Runtime",
-                accessor: "show.runtime"
-              },
-              {
-                Header: "Status",
-                accessor: "show.status"
-              }
-            ]
-          }
+            Header: 'Last Name',
+            accessor: 'lastName',
+          },
         ],
-        []
-    );
+      },
+      {
+        Header: 'Info',
+        columns: [
+          {
+            Header: 'Age',
+            accessor: 'age',
+          },
+          {
+            Header: 'Visits',
+            accessor: 'visits',
+          },
+          {
+            Header: 'Status',
+            accessor: 'status',
+          },
+          {
+            Header: 'Profile Progress',
+            accessor: 'progress',
+          },
+        ],
+      },
+    ],
+    []
+  )
+
+  const dataPresence = React.useMemo(() => makeData(20), [])
+  
+  const initialEmployeeState = {
+    id:           null,
+    matricule:    '',
+    firstname:    '',
+    lastname:     ''
+  }
+
+  const findEmployee = useParams();
+  const employee_id = findEmployee.id 
+  const [loaded, setLoaded] = useState(false)
+  const [dailyemployee, setDailyemployee] = useState(initialEmployeeState)
+  const [data, setData] = useState([])
+
+  
+  useEffect(() => {
+    const load = async () => {
+      const res = await dailyEmployeeService.get(employee_id)
+      console.log("RES LAVA BE ", res.data);
+      setDailyemployee(res.data)
+      setLoaded(true)
+    }
+    if (employee_id && !loaded) {
+      load();
+    }
+  }, [employee_id, loaded])
 
   return (
     <div>
@@ -103,12 +164,14 @@ function EmployeePresence() {
             Listes des présences des employés
         </Typography>
         <Card sx={{ minWidth: 275 }}>
-            <CardContent>
-                <b>N° Matricule : </b>{dailyemployee.matricule} <br/>
-                <b>Nom : </b> {dailyemployee.firstname} <br/>
-                <b>Prénom : </b> {dailyemployee.lastname} <br/>
-            </CardContent>
-            <Table columns={columns} data={data} />
+          <CardContent>
+            <b>N° Matricule : </b>{dailyemployee.matricule} <br/>
+            <b>Nom : </b> {dailyemployee.firstname} <br/>
+            <b>Prénom : </b> {dailyemployee.lastname} <br/>
+          </CardContent>
+          <Styles>
+            <Table columns={columns} data={dataPresence} />
+          </Styles>
         </Card>
     </div>
   )
